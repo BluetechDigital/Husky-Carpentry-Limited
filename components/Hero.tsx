993890c914d2c508302e3d35df4479df27d2b60a
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-03-13 20:13:26 +0000

    Updated the styling.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero: FC<IHero> = ({heroSlider}) => {
 										<SwiperSlide>
 											<div className="w-full p-4">
 												<div
-													className="pt-24 pb-44 sm:pb-20 w-full h-[75vh] sm:h-[65vh] flex flex-col items-center lg:items-baseline justify-center relative bg-center bg-no-repeat bg-cover"
+													className="pt-24 pb-44 sm:pb-20 w-full h-[75vh] sm:h-[75vh] lg:h-[65vh] flex flex-col items-center lg:items-baseline justify-center relative bg-center bg-no-repeat bg-cover"
 													style={{
 														backgroundImage: `linear-gradient(
 																0deg,
@@ -51,22 +51,22 @@ const Hero: FC<IHero> = ({heroSlider}) => {
 															),url("${item?.backgroundImage?.sourceUrl}")`,
 													}}
 												>
-													<div className="relative z-10 flex flex-col items-center lg:items-start gap-4 px-8 px-4 sm:px-24">
+													<div className="relative z-10 flex flex-col items-center lg:items-start gap-4 px-8 px-4 sm:px-8 lg:px-24">
 														<div className="max-w-sm lg:max-w-xl mx-auto lg:mx-0 ">
 															<motion.h1
 																initial={initialTwo}
 																whileInView={fadeIn}
 																viewport={{once: true}}
-																className="text-center lg:text-left uppercase text-lg md:text-xl lg:text-3xl xl:text-5xl tracking-[0.10rem] text-white font-semibold xl:leading-[2.5rem]"
+																className="text-center lg:text-left uppercase text-medium sm:text-lg md:text-xl lg:text-3xl xl:text-5xl tracking-[0.10rem] text-white font-semibold xl:leading-[2.5rem]"
 															>
 																{item?.title}
 															</motion.h1>
 															<Paragraph
 																content={item?.paragraph}
-																tailwindStyling="py-2 text-white leading-[1.75rem] text-base text-center lg:text-left"
+																tailwindStyling="py-2 text-white leading-[1.35rem] sm:leading-[1.75rem] text-base text-center lg:text-left"
 															/>
 														</div>
-														<div className="flex flex-col md:flex-row gap-4 max-w-sm lg:max-w-4xl mx-auto lg:mx-0">
+														<div className="flex flex-col md:flex-row gap-2 sm:gap-4 max-w-sm lg:max-w-4xl mx-auto lg:mx-0">
 															<Link
 																href={`${item?.buttonLink?.url}`}
 																target={item?.buttonLink?.target}
