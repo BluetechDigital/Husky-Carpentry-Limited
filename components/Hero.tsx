35a4e937f8f5035423bbce1f66c9d05de33ab804
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-03-13 19:46:40 +0000

    Updated the styling.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero: FC<IHero> = ({heroSlider}) => {
 										<SwiperSlide>
 											<div className="w-full p-4">
 												<div
-													className="pt-24 pb-20 w-full h-[90vh] sm:h-[65vh] flex flex-col items-center lg:items-baseline justify-center relative bg-center bg-no-repeat bg-cover"
+													className="pt-24 pb-20 w-full h-[100vh] sm:h-[65vh] flex flex-col items-center lg:items-baseline justify-center relative bg-center bg-no-repeat bg-cover"
 													style={{
 														backgroundImage: `linear-gradient(
 																0deg,
